ccd2852bd7cb07ee2b57aea2dbe541d883d1e99f
longshine <longxianghe@gmail.com>
2012-09-30 16:01:55 +0800

    add member and ignore attributes

---
+++ EasyWeb.XmlRpc/XmlRpcIgnoreAttribute.cs
@@ -0,0 +1,24 @@
+﻿//
+// LX.EasyWeb.XmlRpc.XmlRpcIgnoreAttribute.cs
+//
+// Authors:
+//	Longshine He <longshinehe@users.sourceforge.net>
+//
+// Copyright (c) 2012 Longshine He
+//
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT WARRANTY OF ANY KIND.
+//
+
+using System;
+
+namespace LX.EasyWeb.XmlRpc
+{
+    /// <summary>
+    /// Indicates that a field or property should be ignored during XML-RPC serialization.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class XmlRpcIgnoreAttribute : Attribute
+    {
+    }
+}
---
+++ EasyWeb.XmlRpc/XmlRpcMemberAttribute.cs
@@ -0,0 +1,28 @@
+﻿//
+// LX.EasyWeb.XmlRpc.XmlRpcMemberAttribute.cs
+//
+// Authors:
+//	Longshine He <longshinehe@users.sourceforge.net>
+//
+// Copyright (c) 2012 Longshine He
+//
+// This code is distributed in the hope that it will be useful,
+// but WITHOUT WARRANTY OF ANY KIND.
+//
+
+using System;
+
+namespace LX.EasyWeb.XmlRpc
+{
+    /// <summary>
+    /// Determines how a field or property can be serialized.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class XmlRpcMemberAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the name of this member in an XML-RPC request/response.
+        /// </summary>
+        public String Name { get; set; }
+    }
+}
