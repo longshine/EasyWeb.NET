69fe2f6b1aa1474e8ba7e9aaa3dd9f95c79d1e97
longshine <longxianghe@gmail.com>
2012-09-30 17:35:08 +0800

    move system handler into a seperate file

--- EasyWeb.XmlRpc/Server/IXmlRpcSystemHandler.cs
+++ EasyWeb.XmlRpc/Server/IXmlRpcSystemHandler.cs
@@ -18,40 +18,4 @@ namespace LX.EasyWeb.XmlRpc.Server
         /// </summary>
         String GetMethodHelp(String name);
     }
-
-    public abstract class XmlRpcSystemHandlers : IXmlRpcSystemHandler
-    {
-        [XmlRpcMethod(Name = "system.listMethods", IntrospectionMethod = true,
-            Description = "Return an array of all available XML-RPC methods on this service.")]
-        public String[] GetListMethods()
-        {
-            return GetSystemHandler().GetListMethods();
-        }
-
-        [XmlRpcMethod(Name = "system.methodSignature", IntrospectionMethod = true,
-           Description =
-           "Given the name of a method, return an array of legal signatures. " +
-           "Each signature is an array of strings. The first item of each " +
-           "signature is the return type, and any others items are parameter " +
-           "types.")]
-        public String[][] GetMethodSignature(String methodName)
-        {
-            String[][] sig = GetSystemHandler().GetMethodSignature(methodName);
-            if (sig == null)
-                throw new XmlRpcException("No metadata available for method: " + methodName);
-            return sig;
-        }
-
-        [XmlRpcMethod(Name = "system.methodHelp", IntrospectionMethod = true,
-           Description = "Given the name of a method, return a help string.")]
-        public String GetMethodHelp(String methodName)
-        {
-            String help = GetSystemHandler().GetMethodHelp(methodName);
-            if (help == null)
-                throw new XmlRpcException("No help available for method: " + methodName);
-            return help;
-        }
-
-        protected abstract IXmlRpcSystemHandler GetSystemHandler();
-    }
 }
