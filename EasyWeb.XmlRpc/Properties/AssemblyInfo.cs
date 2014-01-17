82a44550a8cab953b95115e7653f4b7d6a24a5c9
longshine <longxianghe@gmail.com>
2014-01-17 16:55:34 +0800

    get signed

--- EasyWeb.XmlRpc/Properties/AssemblyInfo.cs
+++ EasyWeb.XmlRpc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EasyWeb.NET")]
-[assembly: AssemblyCopyright("Copyright © 2009 - 2012 Longshine")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2014 Longshine")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
